57958a1359b3d604f0eea21761630634beb7c40a
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-21 13:53:02 +0900

    Set service name for datadog logging

--- Public/js/logger.js
+++ Public/js/logger.js
@@ -5,6 +5,7 @@ import { datadogLogs } from "@datadog/browser-logs";
 datadogLogs.init({
   clientToken: "pub43c12e5c173400e3670335f5dd0497ca",
   site: "datadoghq.com",
+  service: "swift-format-browser",
   forwardErrorsToLogs: true,
   sampleRate: 100,
 });
